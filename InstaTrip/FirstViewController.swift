503fd523ff26d1411346fab2ed12d58c3a1798e1
Eden Ben Shoshan <edenbenshoshan@edens-imac.local>
2017-12-24 10:55:21 -0800

    Initial Commit

---
+++ InstaTrip/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  InstaTrip
+//
+//  Created by Eden Ben Shoshan on 12/24/17.
+//  Copyright © 2017 Eden Ben Shoshan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ InstaTrip/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  InstaTrip
+//
+//  Created by Eden Ben Shoshan on 12/24/17.
+//  Copyright © 2017 Eden Ben Shoshan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
